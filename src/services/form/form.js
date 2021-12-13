a0ebbb1beafffbc3f4ac782fea40a5a2af79b1b6
Yannick Marcon <yannick.marcon@obiba.org>
2021-12-13 12:03:45 +0100

    crfs service added

--- src/services/form/form.js
+++ src/services/form/form.js
@@ -1,21 +1,10 @@
 import { feathersClient } from '../../boot/feathersClient'
 
 export async function getForms (filter) {
-  const formData = { query: { $sort: { descending: -1 } } }
-  formData.query.$limit = 100
-  // use filter
-  if (filter && filter.length > 1) {
-    formData.query = {
-      $and: [
-        {
-          $or: [
-            { name: { $search: filter } },
-            { description: { $search: filter } }
-          ]
-        }
-      ]
+  const formData = {
+    query: {
+      $limit: 100
     }
   }
-
-  return feathersClient.service('form').find(formData)
+  return feathersClient.service('crfs').find(formData)
 }
