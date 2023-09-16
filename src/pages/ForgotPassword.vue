82f7f12ab50a143d1a9bf17703629637a8558a1a
Yannick Marcon <yannick.marcon@obiba.org>
2023-09-16 17:11:18 +0200

    fix: email validation in forgot password form

--- src/pages/ForgotPassword.vue
+++ src/pages/ForgotPassword.vue
@@ -40,7 +40,8 @@
                     <q-btn
                       :label="$t('forgot_password.submit')"
                       type="submit"
-                      color="dark"/>
+                      color="dark"
+                      :disable="disableSubmit"/>
                     <q-btn
                       :label="$t('forgot_password.login')"
                       flat
@@ -90,7 +91,7 @@ export default {
       submitting: state => state.auth.showLoading
     }),
     disableSubmit () {
-      return this.v$.formData.$invalid
+      return this.v$.resetEmail.$invalid
     }
   },
   methods: {
