9ab7b910259bd01c1be2424dbaa27b137b3ab87e
Yannick Marcon <yannick.marcon@obiba.org>
2023-09-17 11:36:50 +0200

    feat: PATH_PREFIX added

--- quasar.conf.js
+++ quasar.conf.js
@@ -70,7 +70,7 @@ module.exports = configure(function (ctx) {
       },
 
       // transpile: false,
-      // publicPath: '/',
+      publicPath: process.env.PATH_PREFIX ? process.env.PATH_PREFIX : '/',
 
       // Add dependencies for transpiling with Babel (Array of string/regex)
       // (from node_modules, which are by default not transpiled).
