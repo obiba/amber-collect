22a7e4cc6bf4167af916029a9edde4a6c39a6062
Yannick Marcon <yannick.marcon@obiba.org>
2026-06-13 19:18:51 +0200

    feat: removed custom scss

--- quasar.config.js
+++ quasar.config.js
@@ -37,10 +37,7 @@ export default configure(function (ctx) {
     ],
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#css
-    css: [
-      'app.scss',
-      'custom.scss'
-    ],
+    css: ['app.scss'],
 
     // https://github.com/quasarframework/quasar/tree/dev/extras
     extras: [
